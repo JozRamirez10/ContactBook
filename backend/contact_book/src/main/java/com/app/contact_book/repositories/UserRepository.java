cfcedd44ee314e75632391b8e894ee49e26d6cd9
Joz Fama <jm_rg@comunidad.unam.mx>
2025-10-18 13:39:08 -0600

    Add existsByUsername & findByUsername

--- backend/contact_book/src/main/java/com/app/contact_book/repositories/UserRepository.java
+++ backend/contact_book/src/main/java/com/app/contact_book/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.app.contact_book.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.app.contact_book.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 
-    boolean existsById(Long id);
+    Optional<User> findByUsername(String username);
 
+    boolean existsById(Long id);
+    
+    boolean existsByUsername(String username);
 }
